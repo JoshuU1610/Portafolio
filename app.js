71c61f77a2ac3661812927ec5f2babfa66bb418c
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-14 19:16:35 -0400

    cambio del top en el indicador

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 const list = document.querySelectorAll('.list-item');
 const listItems = document.querySelectorAll('.list-item');
-const indicator = document.querySelector('.indicator');
+let indicator = document.querySelector('.indicator');
 
-function setIndicatorPosition() {
-  const activeItem = document.querySelector('.list-item.active');
-  
-  if (activeItem) {
-    const index = Array.from(listItems).indexOf(activeItem);
+    function setIndicatorPosition() {
+        const activeItem = document.querySelector('.list-item.active');
+        
+        if (activeItem) {
+          const index = Array.from(listItems).indexOf(activeItem);
+          const widthValue = activeItem.offsetWidth;
+          const position = index * widthValue;
+          console.log(position);
+          indicator.style.transform = `translateX(${position}px)`;
+        }
+      }
+      setIndicatorPosition();
+
+      listItems.forEach(item => {
+        item.addEventListener('click', () => {
+          listItems.forEach(item => item.classList.remove('active'));
+          
+          item.classList.add('active');
+          
+          setIndicatorPosition();
+        });
+      });
+
+
+
+
+
+
+// listItems.forEach(item => {
+//   item.addEventListener('click', () => {
+//     listItems.forEach(item => item.classList.remove('active'));
     
-    const position = index * 5.3;
-    
-    indicator.style.transform = `translateX(calc(${position}rem))`;
-  }
-}
-
-setIndicatorPosition();
-
-listItems.forEach(item => {
-  item.addEventListener('click', () => {
-    listItems.forEach(item => item.classList.remove('active'));
-    
-    item.classList.add('active');
-    
-    setIndicatorPosition();
-  });
-});
+//     item.classList.add('active');
+//   });
+// });
 
 
-function activelink() {
-    list.forEach(function(item) {
-      item.classList.remove('active');
-    });
-    this.classList.add('active');
-}
+// function activelink() {
+//     list.forEach(function(item) {
+//       item.classList.remove('active');
+//     });
+//     this.classList.add('active');
+// }
 
-list.forEach(e => {
-    e.addEventListener('click', activelink)
-});
+// list.forEach(e => {
+//     e.addEventListener('click', activelink)
+// });
 
