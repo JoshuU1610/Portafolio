de87edf731b4891c6ba4e330fb58d780279db7d6
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-14 16:29:03 -0400

    cambio de diseño

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const list = document.querySelectorAll('.list-item');
 const listItems = document.querySelectorAll('.list-item');
+const indicator = document.querySelector('.indicator');
+
+function setIndicatorPosition() {
+  const activeItem = document.querySelector('.list-item.active');
+  
+  if (activeItem) {
+    const index = Array.from(listItems).indexOf(activeItem);
+    
+    const position = index * 5.3;
+    
+    indicator.style.transform = `translateX(calc(${position}rem))`;
+  }
+}
+
+setIndicatorPosition();
+
+listItems.forEach(item => {
+  item.addEventListener('click', () => {
+    listItems.forEach(item => item.classList.remove('active'));
+    
+    item.classList.add('active');
+    
+    setIndicatorPosition();
+  });
+});
 
 
 function activelink() {
