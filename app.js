3da7d31e4650956bbb4ef618ee5a02259d43a050
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-13 22:59:13 -0400

    cambio de diseño

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const list = document.querySelectorAll('.list-item');
+const listItems = document.querySelectorAll('.list-item');
+
+
+function activelink() {
+    list.forEach(function(item) {
+      item.classList.remove('active');
+    });
+    this.classList.add('active');
+}
+
+list.forEach(e => {
+    e.addEventListener('click', activelink)
+});
+
