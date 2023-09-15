069c7e131d7c2041315207c798162c870d1000ff
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-09-15 18:21:10 -0400

    cambio el codigo js

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const list = document.querySelectorAll('.list-item');
+const list = document.querySelectorAll('#list');
 const listItems = document.querySelectorAll('.list-item');
 let indicator = document.querySelector('.indicator');
 const secciones = document.querySelectorAll('.seccion');
+
 const observer = new IntersectionObserver((entradas, observer) => {
   entradas.forEach(entrada => {
     if (entrada.isIntersecting) {
-      console.log(entrada.target);
+      const targetElement = entrada.target;
+      const parents = Array.from(targetElement.querySelectorAll("*")).filter(element => element !== targetElement);
+      const position = parents.reduce((acc, parent) => {
+        const index = Array.from(parent.children).indexOf(acc.element);
+        return {
+          element: parent,
+          index: index >= 0 ? index : acc.index
+        };
+      }, { element: targetElement, index: Array.from(targetElement.parentNode.children).indexOf(targetElement) });
+
+
+
+      const activeItem = document.querySelector('.list-item.active');
+      const widthValue = activeItem.offsetWidth;
+      const heightValue = activeItem.offsetHeight;
+      const lenght = Number(position.index);
+      const a = lenght * widthValue;
+      const b = lenght * heightValue;
+
+      console.log(lenght);
+      if (window.innerWidth < 1024){
+        indicator.style.transform = `translateX(${a}px)`;
+      }else{
+        indicator.style.transform = `translateY(${b}px)`;
+      }
+      
+      listItems.forEach(item => {
+        item.classList.remove('active');
+        
+        listItems[lenght].classList.add('active');
+    });
+
+      console.log(listItems[lenght]);
     }
   });
 }, {
@@ -40,27 +73,21 @@ function setIndicatorPosition2() {
 
 function handleWindowSize() {
   if (window.innerWidth < 1024) {
-    setIndicatorPosition();
 
     listItems.forEach(item => {
       item.addEventListener('click', () => {
         listItems.forEach(item => item.classList.remove('active'));
         
         item.classList.add('active');
-        
-        setIndicatorPosition();
       });
     });
   } else{
-    setIndicatorPosition();
 
     listItems.forEach(item => {
       item.addEventListener('click', () => {
         listItems.forEach(item => item.classList.remove('active'));
         
         item.classList.add('active');
-        
-        setIndicatorPosition2();
       });
     });
   }
@@ -68,29 +95,3 @@ function handleWindowSize() {
 
 window.addEventListener('resize', handleWindowSize);
 handleWindowSize();
-
-
-
-
-
-
-// listItems.forEach(item => {
-//   item.addEventListener('click', () => {
-//     listItems.forEach(item => item.classList.remove('active'));
-    
-//     item.classList.add('active');
-//   });
-// });
-
-
-// function activelink() {
-//     list.forEach(function(item) {
-//       item.classList.remove('active');
-//     });
-//     this.classList.add('active');
-// }
-
-// list.forEach(e => {
-//     e.addEventListener('click', activelink)
-// });
-
